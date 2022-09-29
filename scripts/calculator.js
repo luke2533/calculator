9a013aaae3e5eaff200e5b9f5baa26b3c441f23a
lukef <lukefoxwell1@gmail.com>
2022-09-29 12:31:43 +0100

    All clear and plusminus functional

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -8,3 +8,14 @@ function input(x) {
         document.getElementById("result").value += x;
     }
 }
+
+function allClear() {
+    document.getElementById("result").value = 0;
+}
+
+function plusminus() {
+    let x = parseFloat(document.getElementById("result").value);
+
+    x *= -1;
+    document.getElementById("result").value = x;
+}
